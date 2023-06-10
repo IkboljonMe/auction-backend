0a3d5ff91ec319f3ec865e4af190bc3fb7297de8
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-06-11 00:05:14 +0200

    try catch added for unhandled err in production

--- base/controllers/upload.js
+++ base/controllers/upload.js
@@ -1,11 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import streamifier from "streamifier";
+
 const uploadFileCloudinary = async (req, res) => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
+
   const streamUpload = (req) => {
     return new Promise((resolve, reject) => {
       const stream = cloudinary.uploader.upload_stream((error, result) => {
@@ -18,7 +20,14 @@ const uploadFileCloudinary = async (req, res) => {
       streamifier.createReadStream(req.file.buffer).pipe(stream);
     });
   };
-  const result = await streamUpload(req);
-  res.send(result);
+
+  try {
+    const result = await streamUpload(req);
+    res.send(result);
+  } catch (error) {
+    console.error("Error occurred during image upload:", error);
+    res.status(500).json({ error: "Failed to upload image" });
+  }
 };
+
 export default uploadFileCloudinary;

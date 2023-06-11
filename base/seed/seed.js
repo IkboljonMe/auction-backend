287da0d79539215e1aeb838c056f44a3a64d970c
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-06-11 02:21:59 +0200

    seed fixed

--- base/seed/seed.js
+++ base/seed/seed.js
@@ -1,17 +1,12 @@
 import express from "express";
-import Product from "../models/productModel.js";
 import User from "../models/userModel.js";
 
 const seed = express.Router();
 
 seed.get("/", async (req, res) => {
-  // removing all previous records in products model
-  await Product.deleteMany({});
-  const createdProducts = await Product.insertMany(data.products);
-
   await User.deleteMany({});
   const createdUsers = await User.insertMany(data.users);
-  res.send({ createdProducts, createdUsers }); // sending new products to the frontend
+  res.send({ createdProducts, createdUsers });
 });
 
 export default seed;
